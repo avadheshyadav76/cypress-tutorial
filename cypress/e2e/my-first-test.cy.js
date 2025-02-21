4d1ac579340c66a43b1dcd31db3edb4847a69fe9
Avadhesh Kumar Yadav <avadhesh.113076@gmail.com>
2025-02-21 13:14:43 +0530

    Initial Code Commit

---
+++ cypress/e2e/my-first-test.cy.js
@@ -0,0 +1,13 @@
+// const cypress = require("cypress");
+
+describe("My First Test", () => {
+  it("Verify title-positive", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM");
+  });
+
+  it("Verify title-negative", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.title().should("eq", "OrangeHRM123");
+  });
+});
